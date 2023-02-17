5283108bf6eb5d4a5b3253503e3e9096eb392164
saikumar7893 <84527251+saikumar7893@users.noreply.github.com>
2023-02-17 10:36:59 +0530

    Add files via upload

---
+++ Queueclass.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class Queueclass {
+public static void main(String args[]) {
+
+    Queue<Integer> que=new LinkedList<>();
+    que.add(1);
+    que.add(2);
+    que.add(3);
+    que.add(4);
+    System.out.println(que.size());
+    System.out.println(que.peek());
+    System.out.println(que.remove());
+    System.out.println(que.size());
+
+
+    
+}
+    
+}
